0ff7ea37b063c475c62e908253753175d459073b
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-08-10 16:53:45 +0800

    Finished set face normal fn.

--- src/hittable.rs
+++ src/hittable.rs
@@ -9,11 +9,23 @@ pub struct HitRecord {
 }
 
 impl HitRecord {
+    /**
+     * Sets the hit record normal vector
+     * NOTE: outward_normal is assumed to be a unit vector.
+     */
     fn set_face_normal(&mut self, r: Ray, outward_normal: Vec3) {
-        self.front_face = r.direction().dot(outward_normal) < 0;
+        self.front_face = r.direction().dot(outward_normal) < 0f64;
+
+        if self.front_face {
+            self.normal = outward_normal
+        }
+        else {
+            self.normal = -outward_normal
+        }
     }
 }
 
 pub trait Hittable {
     fn hit(&self, r: Ray, ray_tmin: f64, ray_tmax: f64, rec: &mut HitRecord) -> bool;
+
 }
