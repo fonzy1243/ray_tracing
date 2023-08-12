be94a42ccb41c412bd08d9751bc080bc2915341a
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-08-12 20:19:34 +0800

    New scene

--- src/main.rs
+++ src/main.rs
@@ -35,9 +35,9 @@ fn main() {
     let mut world = HittableList::default();
 
     let material_ground = Lambertian::new(Color::new(0.8,0.8,0.0));
-    let material_center = Dielectric::new(1.5);
+    let material_center = Lambertian::new(Color::new(0.1,0.2,0.5));
     let material_left = Dielectric::new(1.5);
-    let material_right = Metal::new(Color::new(0.8,0.6,0.2), 1.);
+    let material_right = Metal::new(Color::new(0.8,0.6,0.2), 0.);
 
     world.add(Box::new(Sphere::new(Point3::new(0.,-100.5,-1.), 100., Rc::new(RefCell::new(material_ground)))));
     world.add(Box::new(Sphere::new(Point3::new(0.,0.,-1.), 0.5, Rc::new(RefCell::new(material_center)))));
